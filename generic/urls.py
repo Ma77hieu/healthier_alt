3610947f632f7e4c5a8b6b4d28a81663cb3f1fda
Matthieu O <mat.ortlieb@gmail.com>
2021-04-09 12:06:13 +0200

    index.html now only contents the navbar and footer, home.html extends the index.html to generate the homepage. The templates for each page have been initialized

--- generic/urls.py
+++ generic/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from generic import views
 
 urlpatterns = [
-    path('', views.homepage, name="home"),
+    path('', views.homepage, name="homes"),
     path('legal', views.legal, name="legal")
 ]
--- generic/views.py
+++ generic/views.py
@@ -3,8 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def homepage(request):
+def base(request):
     return render(request, 'index.html')
+
+
+def homepage(request):
+    return render(request, 'home.html')
 
 
 def legal(request):
