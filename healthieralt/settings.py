75aa62b694acd8fadcae00577d3596d59fcdc3bf
Matthieu O <mat.ortlieb@gmail.com>
2021-04-16 11:37:17 +0200

    user model initialization

--- healthieralt/settings.py
+++ healthieralt/settings.py
@@ -114,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'UTC+2'
 
 USE_I18N = True
 
@@ -131,3 +131,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+AUTH_USER_MODEL = 'authentification.User'
