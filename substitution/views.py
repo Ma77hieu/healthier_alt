ac791c124f9a7344fcb842ecd23f2bff71658815
Matthieu O <mat.ortlieb@gmail.com>
2021-04-20 12:20:33 +0200

    user favorites page now available baseurl/user/user_id/favorites

--- substitution/views.py
+++ substitution/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from substitution.models import Product
+from substitution.models import Product, Favorites
 
 
 def results(request):
@@ -10,3 +10,13 @@ def results(request):
 def details(request, product_id):
     product = Product.objects.get(pk=product_id)
     return render(request, 'detailprod.html', {'product': product})
+
+
+def favorites(request, user):
+    user_favs = Favorites.objects.filter(user_id=user)
+    favs_ids_list = []
+    for fav in user_favs:
+        favs_ids_list.append(fav.product_id)
+    user_fav_prod = Product.objects.filter(pk__in=favs_ids_list)
+    # user_fav_prod = Product.objects.get(pk=2)
+    return render(request, 'favorites.html', {'fav_products': user_fav_prod})
