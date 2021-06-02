88951bcb9770edf0db4f4238e926acf7592bca59
Matthieu O <mat.ortlieb@gmail.com>
2021-06-02 10:27:01 +0200

    error management improvement

--- authentification/views.py
+++ authentification/views.py
@@ -1,12 +1,9 @@
 from django.shortcuts import render, redirect
-# from authentification.models import User
 from authentification.forms import UserForm
-# from authentification.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import User
-# from django.contrib.auth import set_password
 
 
 # Create your views here.
@@ -47,21 +44,7 @@ def signup(request):
             print('form: {}'.format(form))
             form_update = form.save(commit=False)
             form_update.password = make_password(form.cleaned_data['password'])
-            # pwd = make_password(form.cleaned_data['password'])
-            # print("PASSWORD: {}".format(pwd))
-            # print("form.password avant: {}".format(form))
-            # # form.cleaned_data['password'] = pwd
-            # print("form.password apres: {}".format(form))
             form_update.save()
-            # form = up_form(request.POST or None)
-            # if form.is_valid():
-            #     sign_up = form.save(commit=False)
-            #     sign_up.password = make_password(form.cleaned_data['password'])
-            #     sign_up.status = 1
-            #     sign_up.save()
-            #     return redirect('/thank-you/')
-
-            # print('PASSWORD AUTH:{}'.format(form.cleaned_data['password']))
             user_name = form.cleaned_data['username']
             pwd = form.cleaned_data['password']
             signup = True
@@ -70,8 +53,9 @@ def signup(request):
         else:
             messages.error(
                 request, "Unsuccessful registration. Invalid information.")
+            signup = False
 
-    return render(request, 'signin.html', {'form': form})
+    return render(request, 'signin.html', {'form': form, 'signup': signup})
 
 
 def signin(request):
@@ -85,9 +69,6 @@ def signin(request):
         user_name = request.POST['username']
         pwd = request.POST['password']
         signup = False
-        # auth = authenticate(request, username=user_name,
-        #                     password=pwd)
-        # print("AUTH:{}".format(auth))
         login_user(request, user_name, pwd, signup)
         return redirect('home')
     return render(request, 'signin.html', {'form': form})
