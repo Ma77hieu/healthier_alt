6f48c500dfa801dfe9c896ec7ed046efbb625ade
Matthieu O <mat.ortlieb@gmail.com>
2021-06-04 06:37:41 +0200

    authentification related views are now handling only the renders, all code logic managed in services.py

---
+++ authentification/services.py
@@ -0,0 +1,66 @@
+from django.shortcuts import render, redirect
+from authentification.forms import UserForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import User
+from substitution.constants import LOG_IN_OK, LOG_OUT_OK, INVALID_CREDENTIALS
+
+
+# Create your views here.
+
+def login_user(request, user_name, pwd, signup):
+    """function used to authenticate a user.
+    This function is called when user signin and/or signup"""
+    auth = authenticate(request, username=user_name,
+                        password=pwd)
+    if auth is not None:
+        logged_user = login(request, auth)
+        if signup == True:
+            html_page = 'signin.html'
+        else:
+            html_page = 'user.html'
+        return render(request, html_page, {
+            'form': logged_user})
+    else:
+        return INVALID_CREDENTIALS
+
+
+def signup_service(request):
+    """function called when a user register to the website"""
+    if request.method == "GET":
+        form = UserForm()
+    elif request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form_update = form.save(commit=False)
+            form_update.password = make_password(form.cleaned_data['password'])
+            form_update.save()
+            user_name = form.cleaned_data['username']
+            pwd = form.cleaned_data['password']
+            signup = True
+            login_user(request, user_name, pwd, signup)
+
+        else:
+            messages.error(
+                request, "Unsuccessful registration. Invalid information.")
+            signup = False
+
+    return ({'form': form, 'signup': signup})
+
+
+def signin_service(request):
+    """function called when a user log-in to the website"""
+    if request.method == "GET":
+        form = UserForm()
+        return ('signin.html', {'form': form})
+    elif request.method == "POST":
+        user_name = request.POST['username']
+        pwd = request.POST['password']
+        signup = False
+        login = login_user(request, user_name, pwd, signup)
+        if login == INVALID_CREDENTIALS:
+            form = UserForm()
+            return ('signin.html', {'form': form, 'error': INVALID_CREDENTIALS})
+        else:
+            return ('user.html', {'user_message': LOG_IN_OK})
--- authentification/views.py
+++ authentification/views.py
@@ -1,94 +1,32 @@
-from django.shortcuts import render, redirect
-from authentification.forms import UserForm
-from django.contrib import messages
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.hashers import make_password
-from django.contrib.auth.models import User
-from substitution.constants import LOG_IN_OK, LOG_OUT_OK, INVALID_CREDENTIALS
+from django.shortcuts import render
+from django.contrib.auth import logout
+from substitution.constants import LOG_OUT_OK
+
+from .services import signup_service, signin_service
 
 
 # Create your views here.
 
-def login_user(request, user_name, pwd, signup):
-    print(user_name)
-    print(pwd)
-
-    auth = authenticate(request, username=user_name,
-                        password=pwd)
-    print("AUTH:{}".format(auth))
-    if auth is not None:
-        logged_user = login(request, auth)
-        if signup == True:
-            html_page = 'signin.html'
-        else:
-            html_page = 'user.html'
-        return render(request, html_page, {
-            'form': logged_user})
-    else:
-        print(INVALID_CREDENTIALS)
-        return INVALID_CREDENTIALS
-        # print(
-        #     request, "Unsuccessful registration. Invalid Credentials.")
-        # messages.error(
-        #     request, "Unsuccessful registration. Invalid Credentials.")
-
-
 def signup(request):
-    if request.method == "GET":
-        form = UserForm()
-    elif request.method == "POST":
-        print("----\nSIGNUP\n----")
-        print("REQUEST.POST:{}".format(request.POST))
-        # print("request username:{}".format(request.POST["username"]))
-        # print("request password:{}".format(request.POST["password"]))
-
-        form = UserForm(request.POST)
-        if form.is_valid():
-            print('form: {}'.format(form))
-            form_update = form.save(commit=False)
-            form_update.password = make_password(form.cleaned_data['password'])
-            form_update.save()
-            user_name = form.cleaned_data['username']
-            pwd = form.cleaned_data['password']
-            signup = True
-            login_user(request, user_name, pwd, signup)
-
-        else:
-            messages.error(
-                request, "Unsuccessful registration. Invalid information.")
-            signup = False
-
-    return render(request, 'signin.html', {'form': form, 'signup': signup})
+    """view when a user register to the website"""
+    context = signup_service(request)
+    return render(request, 'signin.html', context)
 
 
 def signin(request):
-    if request.method == "GET":
-        form = UserForm()
-        return render(request, 'signin.html', {'form': form})
-    elif request.method == "POST":
-        print("----\nSIGNIN\n----")
-        print("REQUEST.POST:{}".format(request.POST))
-        # print("request username:{}".format(request.POST["username"]))
-        # print("request password:{}".format(request.POST["password"]))
-        user_name = request.POST['username']
-        pwd = request.POST['password']
-        signup = False
-        login = login_user(request, user_name, pwd, signup)
-        if login == INVALID_CREDENTIALS:
-            form = UserForm()
-            return render(request, 'signin.html', {'form': form, 'error': INVALID_CREDENTIALS})
-        else:
-            return render(request, 'home.html', {'user_message': LOG_IN_OK})
+    """view managing user log-in to the website"""
+    html_page = signin_service(request)[0]
+    context = signin_service(request)[1]
+    return render(request, html_page, context)
 
 
 def logout_user(request):
-    print("REQUEST.USER: {}".format(request.user))
+    """view managing user log-out"""
     logout(request)
-    print("user logged out")
-    print("REQUEST.USER: {}".format(request.user))
     user_message = LOG_OUT_OK
     return render(request, 'home.html', {'user_message': user_message})
 
 
 def user(request):
+    """view managing the acess to the user's page"""
     return render(request, 'user.html')
