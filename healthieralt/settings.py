075819face373303ae761fb84d610dd0a41dc286
Matthieu O <mat.ortlieb@gmail.com>
2021-06-15 09:46:35 +0200

    Heroku deployment preparation, add procfile, use django_heroku lib, requirements.txt update

--- healthieralt/settings.py
+++ healthieralt/settings.py
@@ -9,10 +9,10 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
 from pathlib import Path
 from decouple import config
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -137,3 +137,6 @@ STATICFILES_DIRS = (
 )
 
 AUTH_USER_MODEL = 'authentification.User'
+
+# Configure Django App for Heroku.
+django_heroku.settings(locals())
